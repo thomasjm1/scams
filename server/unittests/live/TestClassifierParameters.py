a3dfdd95dd254614af163ac89c487f1c648fc106
Jeremy Thomas <thomasjm@andrew.cmu.edu>
2018-04-15 10:37:55 -0400

    Add live test for classifier parameters.

--- server/unittests/live/TestClassifierParameters.py
+++ server/unittests/live/TestClassifierParameters.py
@@ -1,18 +1,27 @@
-import logging
+import json
 import unittest
 
-import sys
-
 import requests
-
-from app import AppFactory
 
 
 class TestClassifierParameters(unittest.TestCase):
 
     def test_index(self):
-        requests.post('http://httpbin.org/post', data={'key': 'value'})
+        data = {
+            # Any structure for content
+            'content': {
+                'words': ['hello', 'world'],
+                'more': 'important information'
+            },
+            # Hardcoded key
+            'key': 'p4ZfOJxhXJOU5VE9mdPX8Mo5V8dveda1bCUQaQ4QzHo06nrklJxRvdNpUZSE4WnG'
+        }
+        response = requests.post(
+            'https://eps-scams.appspot.com/api/parameters/',
+            json=data)
         self.assertTrue(response.status_code == 200)
+        json_body = response.json()
+        self.assertTrue(json_body['message'] == 'classifier parameters updated')
 
 
 if __name__ == '__main__':
